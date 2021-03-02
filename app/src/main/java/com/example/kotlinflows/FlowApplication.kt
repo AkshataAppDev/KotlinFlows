3759110bc804b53fce812d9feac5b690488edaeb
Akshata Deshpande <ad.march6@gmail.com>
2021-03-02 11:43:11 +0000

    Added System Property to printout names for Coroutines.

--- app/src/main/java/com/example/kotlinflows/FlowApplication.kt
+++ app/src/main/java/com/example/kotlinflows/FlowApplication.kt
@@ -6,6 +6,7 @@ class FlowApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // Required to printout coroutines names like [main @coroutine#2], [main @coroutine#1]
         System.setProperty("kotlinx.coroutines.debug", if (BuildConfig.DEBUG) "on" else "off")
 
     }
