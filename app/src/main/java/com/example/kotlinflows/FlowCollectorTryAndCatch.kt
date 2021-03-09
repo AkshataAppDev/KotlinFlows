10201ce0697116cf8170fafff7e995133ebd9071
Akshata Deshpande <ad.march6@gmail.com>
2021-03-09 15:35:39 +0000

    Flow try-catch in collect

---
+++ app/src/main/java/com/example/kotlinflows/FlowCollectorTryAndCatch.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinflows
+
+import android.os.Bundle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import timber.log.Timber
+
+class FlowCollectorTryAndCatch : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        runBlocking {
+
+            try {
+
+                simple().collect { value ->
+                    Timber.d("Printing $value")
+                    // kotlin's check method.
+                    check(value <= 1) { "Collected $value" }
+                }
+
+            } catch (e: Throwable) {
+                Timber.e("Caught $e")
+            }
+        }
+    }
+
+    fun simple(): Flow<Int> = flow {
+        for (i in 1..3) {
+            Timber.d("Emitting $i")
+            emit(i)
+        }
+    }
+
+}
