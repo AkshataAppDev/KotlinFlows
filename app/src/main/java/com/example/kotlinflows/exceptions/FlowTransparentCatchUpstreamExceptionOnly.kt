45b11b2d8e0085e60b1d9506d14b0a449390305e
Akshata Deshpande <ad.march6@gmail.com>
2021-03-11 09:30:51 +0000

    Catching upstream exception honoring exception transparency.

--- app/src/main/java/com/example/kotlinflows/exceptions/FlowTransparentCatchUpstreamExceptionOnly.kt
+++ app/src/main/java/com/example/kotlinflows/exceptions/FlowTransparentCatchUpstreamExceptionOnly.kt
@@ -2,10 +2,35 @@ package com.example.kotlinflows.exceptions
 
 import android.os.Bundle
 import com.example.kotlinflows.ui.base.BaseActivity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import timber.log.Timber
 
-class FlowTransparentCatchUpstreamExceptionOnly: BaseActivity(){
+class FlowTransparentCatchUpstreamExceptionOnly : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        runBlocking {
+            simple()
+                .catch { e -> Timber.e("Caught $e") } // this is not printed because catch operator honors exception transparency and catches only upstream exception i.e exception from all operators above catch
+                .collect { value ->
+                    check(value <= 1) { "Collected $value" }
+                    Timber.d("Value $value")
+                }
+        }
     }
+
+    fun simple(): Flow<Int> = flow {
+
+        for (i in 1..3) {
+            Timber.d("Emitting $i")
+            emit(i)
+        }
+
+    }
+
 }
